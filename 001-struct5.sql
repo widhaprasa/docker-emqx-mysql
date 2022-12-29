e93428ad4a795583b677922d5a38eb54d0be25e4
widhaprasa <widhaprasa@gmail.com>
2022-12-29 13:33:04 +0700

    Add gitignore Update structure

--- 001-struct5.sql
+++ 001-struct5.sql
@@ -4,9 +4,9 @@ GRANT ALL PRIVILEGES ON emqx_mysql.* TO 'emqx_mysql'@'%';
 -- Use database
 USE emqx_mysql;
 
--- Create default tablex
+-- Create default tables and indexes
 CREATE TABLE `mqtt_user` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `peerhost` varchar(60) DEFAULT NULL,
   `clientid` varchar(100) DEFAULT NULL,
   `username` varchar(100) DEFAULT NULL,
@@ -18,17 +18,18 @@ CREATE TABLE `mqtt_user` (
   `created` datetime DEFAULT NULL,
   `group_` varchar(35) DEFAULT NULL,
   `listener` varchar(35) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `mqtt_username` (`username`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE INDEX mqtt_user_peerhost_idx ON mqtt_user(peerhost, listener);
-CREATE INDEX mqtt_user_clientid_idx ON mqtt_user(clientid, listener);
-CREATE INDEX mqtt_user_username_idx ON mqtt_user(username, listener);
-CREATE INDEX mqtt_user_group_idx ON mqtt_user(group_, listener);
+CREATE INDEX mqtt_user_listener_idx ON mqtt_user(listener);
+
+CREATE INDEX mqtt_user_peerhost_listener_idx ON mqtt_user(peerhost, listener);
+CREATE INDEX mqtt_user_clientid_listener_idx ON mqtt_user(clientid, listener);
+CREATE INDEX mqtt_user_username_listener_idx ON mqtt_user(username, listener);
+CREATE INDEX mqtt_user_group_listener_idx ON mqtt_user(group_, listener);
 
 CREATE TABLE `mqtt_acl` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `peerhost` varchar(60) DEFAULT NULL,
   `clientid` varchar(100) DEFAULT NULL,
   `username` varchar(100) DEFAULT NULL,
@@ -39,11 +40,15 @@ CREATE TABLE `mqtt_acl` (
   `topic` varchar(100) NOT NULL,
   `group_` varchar(35) DEFAULT NULL,
   `listener` varchar(35) DEFAULT NULL,
-  INDEX username_idx(username),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE INDEX mqtt_acl_peerhost_idx ON mqtt_acl(peerhost, listener);
-CREATE INDEX mqtt_acl_clientid_idx ON mqtt_acl(clientid, listener);
-CREATE INDEX mqtt_acl_username_idx ON mqtt_acl(username, listener);
-CREATE INDEX mqtt_acl_group_idx ON mqtt_acl(group_, listener);
+CREATE INDEX mqtt_acl_listener_idx ON mqtt_acl(listener);
+CREATE INDEX mqtt_acl_peerhost_idx ON mqtt_acl(peerhost);
+CREATE INDEX mqtt_acl_clientid_idx ON mqtt_acl(clientid);
+CREATE INDEX mqtt_acl_username_idx ON mqtt_acl(username);
+
+CREATE INDEX mqtt_acl_peerhost_listener_idx ON mqtt_acl(peerhost, listener);
+CREATE INDEX mqtt_acl_clientid_listener_idx ON mqtt_acl(clientid, listener);
+CREATE INDEX mqtt_acl_username_listener_idx ON mqtt_acl(username, listener);
+CREATE INDEX mqtt_acl_group_listener_idx ON mqtt_acl(group_, listener);
